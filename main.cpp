014c48a81593dcc9e2b4c06c75f6cb67acf65486
jaehong <jaehong8754@gmail.com>
2020-08-30 07:25:46 -0700

    test vscode and git

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, const char * argv[])
 {
     int i = 0;
     std::cout << "test cmake and vscode!!!" << std::endl;
+    std::cout << "test vscode and git!!!" << std::endl;
     
     printf("%s\n", func_a().c_str());
     printf("%s\n", func_c().c_str());
