9e2fc7b512fddab15da4d0f3c0389feb92fda3e4
jaehong <jaehong8754@gmail.com>
2020-08-30 07:12:47 -0700

    cmake and vscode setting record

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <a.h>
+#include <c/c.h>
+#include <d.h>
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    int i = 0;
+    std::cout << "test cmake and vscode!!!" << std::endl;
+    
+    printf("%s\n", func_a().c_str());
+    printf("%s\n", func_c().c_str());
+    printf("%s\n", func_d().c_str());    
+
+    return 0;
+}
